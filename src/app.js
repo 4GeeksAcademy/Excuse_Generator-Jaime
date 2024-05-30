41d8d6a933e684d48d17ec938756557b84860978
mecrophagus <122439515+mecrophagus@users.noreply.github.com>
2024-05-30 05:33:28 +0000

    Terminando

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  setExcuse();
 };
+
+function setExcuse() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  let excuse =
+    randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
+
+  document.getElementById("excuse").innerHTML = excuse;
+}
+document.querySelector("#btn").addEventListener("click", () => {
+  setExcuse();
+});
